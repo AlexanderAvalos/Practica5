6e4f3236b12daf9d8c101bb3ef442e0b8b611419
Alex <3032801820108@ingenieria.usac.edu.gt>
2023-06-19 17:29:29 -0600

    Practica 6

--- practica/cliente/index.js
+++ practica/cliente/index.js
@@ -1,21 +1,23 @@
-const clienteRouter = require('./routes/cliente.route');
+const clienteRouter = require("./routes/cliente.route");
 
-var express = require('express');
-const bodyParser = require('body-parser');
+var express = require("express");
+const bodyParser = require("body-parser");
 
 const port = 3002;
 
 var app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/cliente', clienteRouter);
+app.use("/cliente", clienteRouter);
 
-app.get('/', function (req, res) {
-    res.send('hello, world!')
-  })
+app.get("/", function (req, res) {
+  res.send("hello, world!");
+});
 
 app.listen(port, function () {
-    console.log(`Server on port http://localhost:${port}`);
-  });
+  console.log(`Server on port http://localhost:${port}`);
+});
+
+module.exports = app;
